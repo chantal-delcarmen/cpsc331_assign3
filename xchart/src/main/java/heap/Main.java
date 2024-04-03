fdb02528f5c8bb0f2a2e8fb7af8cf83c999ca75f
Chantal <104171352+RNBVE@users.noreply.github.com>
2024-04-02 18:16:43 -0600

    problem 3 - optimizing

--- xchart/src/main/java/heap/Main.java
+++ xchart/src/main/java/heap/Main.java
@@ -9,13 +9,14 @@ public class Main {
 	private static int randomHeapifySwapCounter = 0;
 	private static int sortedOneByOneSwapCounter = 0;
 	private static int randomOneByOneSwapCounter = 0;	
-	private static int[] sortedArr = new int[maxSize];
-	private static int[] randomArr = new int [maxSize];
+	private static int[] sortedArray = new int[maxSize];
+	private static int[] randomArray = new int [maxSize];
 	private static int[] sortedHeapifyHeap = new int[maxSize];
 	private static int[] randomHeapifyHeap = new int[maxSize];
-	private static int[] sortedOneByoneHeap = new int[maxSize];
-	private static int[] randomOneByoneHeap = new int[maxSize];
+	private static int[] sortedOneByOneHeap = new int[maxSize];
+	private static int[] randomOneByOneHeap = new int[maxSize];
 	private static int size = 0;
+	private static Random rand = new Random();
 	
     // Returns the index of the parent for the node at index pos
     private static int parent(int pos) {return pos / 2; }
@@ -28,120 +29,30 @@ public class Main {
 
     // Checks if the node at index pos is a leaf node
     private static boolean isLeaf(int pos) { return pos > (size / 2) && pos <= size; }
-	
-	public static void main(String[] args) {
+
+
+	private static int[] getRandomArray(int length) {
+		int[] array = new int[length];
 		
-		// -----------------------------------------------------------------
-		// HEAPIFY 
-		
-		// Populate sorted array
-		for(int i = 0; i < sortedArr.length; i++) {
-			sortedArr[i] = i;
+		for(int i = 0; i < length; i++) {	
+			array[i] = rand.nextInt(1000);
 		}
-
-		// Populate random array
-		Random rand = new Random();
-		for(int i = 0; i < randomArr.length; i++) {	
-			randomArr[i] = rand.nextInt(1000);
-		}
-		
-		// Set swapCounter to 0 and build heap for sorted array
-		swapCounter = 0;
-		sortedHeapifyHeap = buildHeap(sortedArr, sortedArr.length);
-		sortedHeapifySwapCounter = swapCounter;
-		
-		// Reset swapCounter to 0 and build heap for random array
-		swapCounter = 0; 
-		randomHeapifyHeap = buildHeap(randomArr, randomArr.length);
-		randomHeapifySwapCounter = swapCounter;
-		
-		// Output
-		System.out.println("HEAPIFY");
-		
-		System.out.println("INPUT: A random array with a size of 1000");
-		System.out.println(randomHeapifySwapCounter + "\n");
-		
-		System.out.println("INPUT: A sorted array with a size of 1000");
-		System.out.println(sortedHeapifySwapCounter + "\n");
-		
-		// -----------------------------------------------------------------
-		// ONE-BY-ONE 
-		
-		System.out.println("ONE BY ONE\n");
-
-		// Reset swap counter again
-		swapCounter = 0; 
-		size = 0;
-		
-//		System.out.println("SORTED ONE BY ONE\n");
-
-		for(int i = 0; i < maxSize; i++) {
-			insert(sortedOneByoneHeap, i);
-		}
-		
-		sortedOneByOneSwapCounter = swapCounter;
-		
-		// Reset swap counter again
-		swapCounter = 0;
-		size = 0;
-		
-//		System.out.println("RANDOM ONE BY ONE\n");
-
-		for(int i = 0; i < maxSize; i++) {
-			insert(randomOneByoneHeap, rand.nextInt(1000));
-		}
-		
-		randomOneByOneSwapCounter = swapCounter;
-		
-		// Output
-
-		System.out.println("INPUT: A random array with a size of 1000");
-		System.out.println(randomOneByOneSwapCounter + "\n");
-		
-		System.out.println("INPUT: A sorted array with a size of 1000");
-		System.out.println(sortedOneByOneSwapCounter + "\n");
-		
-	} // End main method
-
-	private static void insert(int[] heap, int data) {
-//		if(isFull()) {
-//			return;
-//		}
-//		
-//		// Add element at position heap[size] aka at end of array
-//		heap[size] = data;
-//		
-//		// Set initial current and parent node
-//		int current = size;
-//		
-//		// While current is > 0 (not the root) AND the difference 
-//		// between the value of current and value of parent is > 0
-//        // Move the new element up to maintain the heap property
-//        while (heap[current] > heap[parent(current)]) {
-//            swap(current, parent(current));
-//            current = parent(current);
-//        }
-//		System.out.print(size + "/" + maxSize + " | ");
-		
-        if (size >= maxSize) {
-        	System.out.println("Heap is full!");
-            return; // Heap is full
-        }
-        
-        heap[size] = data;
-        int current = size;
-        
-
-
-        // Move the new element up to maintain the heap property
-        while (heap[current] > heap[parent(current)]) {
-            swap(heap, current, parent(current));
-            current = parent(current);
-        }	
-        size++;
+		return array;
 	}
 
+	private static int[] getSortedArray(int length) {
+		int[] array = new int[length];
+		
+		for(int i = 0; i < length; i++) {
+			array[i] = i;
+		}
+		
+		return array;
+	}
+	
 	private static int[] buildHeap(int[] array, int length) {
+		swapCounter = 0;
+
 		int start = (length/2) - 1;
 		
 		for(int i = start; i >=0; i--) {
@@ -178,7 +89,102 @@ public class Main {
 		heap[location] = heap[parent];
 		heap[parent] = temp;		
 		swapCounter++;
-		
-//		System.out.println("Swapping " + heap[parent] + " and " + heap[location]);
 	}
+	
+	private static void insert(int[] heap, int data) {
+		
+        if (size >= maxSize) {
+        	System.out.println("Heap is full!");
+            return; // Heap is full
+        }
+        
+        heap[size] = data;
+        int current = size;
+        
+        // Move the new element up to maintain the heap property
+        while (heap[current] > heap[parent(current)]) {
+            swap(heap, current, parent(current));
+            current = parent(current);
+        }	
+        size++;
+	}
+	
+
+	private static int[] buildRandomOneByOne() {
+		// Reset swap counter again
+		swapCounter = 0;
+		size = 0;
+		
+		int[] array = new int[maxSize];
+
+		for(int i = 0; i < maxSize; i++) {
+			insert(array, rand.nextInt(1000));
+		}
+		
+		return array;
+	}
+
+	private static int[] buildSortedOneByOne() {
+		// Reset swap counter again
+		swapCounter = 0; 
+		size = 0;
+
+		int[] array = new int[maxSize];
+		
+		for(int i = 0; i < maxSize; i++) {
+			insert(array, i);
+		}
+				
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		
+		// -----------------------------------------------------------------
+		// HEAPIFY 
+		
+		// Create / populate sorted array
+		sortedArray = getSortedArray(sortedArray.length);
+
+		// Create / populate random array
+		randomArray = getRandomArray(randomArray.length);
+		
+		// Build heap for sorted array
+		sortedHeapifyHeap = buildHeap(sortedArray, sortedArray.length);
+		sortedHeapifySwapCounter = swapCounter;
+		
+		// Build heap for random array
+		randomHeapifyHeap = buildHeap(randomArray, randomArray.length);
+		randomHeapifySwapCounter = swapCounter;
+		
+		// Output
+		System.out.println("HEAPIFY");
+		
+		System.out.println("INPUT: A random array with a size of 1000");
+		System.out.println(randomHeapifySwapCounter + "\n");
+		
+		System.out.println("INPUT: A sorted array with a size of 1000");
+		System.out.println(sortedHeapifySwapCounter + "\n");
+		
+		// -----------------------------------------------------------------
+		// ONE-BY-ONE 
+		
+		System.out.println("ONE BY ONE\n");
+
+		sortedOneByOneHeap = buildSortedOneByOne();
+		sortedOneByOneSwapCounter = swapCounter;
+		
+		randomOneByOneHeap = buildRandomOneByOne();
+		randomOneByOneSwapCounter = swapCounter;
+		
+		// Output
+
+		System.out.println("INPUT: A random array with a size of 1000");
+		System.out.println(randomOneByOneSwapCounter + "\n");
+		
+		System.out.println("INPUT: A sorted array with a size of 1000");
+		System.out.println(sortedOneByOneSwapCounter + "\n");
+		
+	} // End main method
+
 }
